bb77644a7a679c9f9d744335ed4de8d1d04852a0
Anakonkai <nguyentranhoangnhan18@gmail.com>
2024-05-08 13:52:15 +0700

    hoan thanh cac class lien quan chua lam Reservation System

---
+++ Java Assignment Require/Assignment_OOP_2024/SourceCode/CommonAccommodation.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class CommonAccommodation extends Accommodation {
+   private ArrayList<Room> rooms;
+   private float rating_Coefficient;
+
+   public CommonAccommodation(int iD_Accommodation, String name_Accommodation, String address_Accommodation,
+         String city_Accommodation, ArrayList<Room> rooms, float rating_Coefficient) {
+      super(iD_Accommodation, name_Accommodation, address_Accommodation, city_Accommodation);
+      this.rooms = rooms;
+      this.rating_Coefficient = rating_Coefficient;
+   }
+
+   public CommonAccommodation(int iD_Accommodation, String name_Accommodation, String address_Accommodation,
+         String city_Accommodation, float rating_Coefficient) {
+      super(iD_Accommodation, name_Accommodation, address_Accommodation, city_Accommodation);
+      this.rooms = new ArrayList<>();
+      this.rating_Coefficient = rating_Coefficient;
+   }
+
+   public ArrayList<Room> getRooms() {
+      return rooms;
+   }
+
+   public void setRooms(ArrayList<Room> rooms) {
+      this.rooms = rooms;
+   }
+
+   public float getRating_Coefficient() {
+      return rating_Coefficient;
+   }
+
+   public void setRating_Coefficient(float rating_Coefficient) {
+      this.rating_Coefficient = rating_Coefficient;
+   }
+}
